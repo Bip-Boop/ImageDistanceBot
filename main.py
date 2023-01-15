28fc4d8a0429bb85c06e426a1ee4313ca2355ba0
Bip-Boop <62621659+Bip-Boop@users.noreply.github.com>
2023-01-15 17:34:54 +0300

    Telegram

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from image_math import ImageMath
 from telegram.ext import *
 import telegram
 
+im = None
 user_ids_even = dict()
 def start_command(update, context):
     name = update.message.chat.first_name
@@ -12,19 +13,35 @@ def start_command(update, context):
 
 def image_handler(update, context):
     global user_ids_even
-    if update.message.chat_id in user_ids_even:
-        user_ids_even[update.message.chat_id] = not user_ids_even[update.message.chat_id]
+    global im
+    uid = update.message.chat_id
+    if uid in user_ids_even:
+        user_ids_even[uid] = not user_ids_even[uid]
     else:
-        user_ids_even[update.message.chat_id] = False
+        user_ids_even[uid] = False
     
     file = update.message.photo[-1].get_file()
-    path = file.download(f"output{update.message.chat_id}{int(user_ids_even[update.message.chat_id])}.jpg")
-    update.message.reply_text("Image received")
-    update.message.reply_text("Hello " + str(update.message.chat_id))
+    path = file.download(f"output{uid}{int(user_ids_even[uid])}.jpg")
+
+    if user_ids_even[uid] == False:
+        update.message.reply_text("First image received. Need one more to compare")
+    else:
+        percentage = (im.calculate_conscious_distance(f"output{uid}0.jpg", f"output{uid}1.jpg")) * 100
+        print(percentage, 100-percentage)
+        update.message.reply_text(f"Similarity: {str(100.0- percentage)[:5]}%")
+        distance = im.calculate_subconscious_distance(f"output{uid}0.jpg", f"output{uid}1.jpg")
+        update.message.reply_text(f"Distance: {int(distance)} units")
+
+    
+
+
 
 def main():
+    global im
+    im = ImageMath()
+
     print("Started")
-    TOKEN = "token"
+    TOKEN = ""
     updater = Updater(TOKEN, use_context = True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start_command))
